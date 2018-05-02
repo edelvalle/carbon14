96c2ab80d95feb58c037980a936ed1832da5d9f7
Eddy Ernesto del Valle Pino <xigmatron@gmail.com>
2018-05-02 17:51:14 +0200

    Make carbon-django independent from rest-framework

--- carbon14/django.py
+++ carbon14/django.py
@@ -1,32 +1,18 @@
+import json
+
 from functools import partial
 
-from django.conf import settings
+from django import forms
 from django.db.models import QuerySet
-
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework.parsers import BaseParser
-from rest_framework.renderers import JSONRenderer
-from rest_framework.exceptions import ParseError
+from django.db.models.fields.files import FieldFile
+from django.http import HttpResponse, JsonResponse
+from django.views.generic import View
+from django.template import Template, RequestContext
+from django.core.serializers.json import DjangoJSONEncoder
 
 from .graphql import parse
 from .errors import Carbon14Error
 from .neonode import RootNode, Node
-
-
-class GraphQLParser(BaseParser):
-
-    media_type = 'application/graphql'
-    renderer_class = JSONRenderer
-
-    def parse(self, stream, media_type=None, parser_context=None):
-        parser_context = parser_context or {}
-        encoding = parser_context.get('encoding', settings.DEFAULT_CHARSET)
-        try:
-            data = stream.read().decode(encoding)
-            return parse(data)
-        except Carbon14Error as e:
-            raise ParseError(str(e))
 
 
 class ModelNode(Node):
@@ -94,21 +80,91 @@ class ModelNode(Node):
         return str(item.id)
 
 
-class GraphQLView(APIView):
+class CarbonJSONEncoder(DjangoJSONEncoder):
+    def default(self, o):
+        if isinstance(o, FieldFile):
+            return o.url
+        return super().default(o)
 
-    parser_classes = (
-        GraphQLParser,
-    )
+
+class GrapQLForm(forms.Form):
+    query = forms.CharField(widget=forms.Textarea)
+
+
+class GraphQLView(View):
+
+    template = Template('''
+        <!DOCTYPE html>
+        <html>
+        <head>
+          <meta charset="utf-8" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+          <meta
+            name="viewport"
+            content="width=device-width,
+            initial-scale=1,
+            minimum-scale=1,
+            maximum-scale=1"
+          >
+          <title>Carbon14</title>
+        </head>
+        <body>
+
+          <form method="post">
+            {{ form }}
+            {% csrf_token %}
+            <input type="submit" value="Query">
+          </form>
+
+          <pre>{{ answer|safe }}</pre>
+
+        </body>
+        </html>
+
+    ''')
+    encoder_class = CarbonJSONEncoder
     nodes = tuple()
 
+    def get(self, request):
+        return HttpResponse(content=self.render(form=GrapQLForm()))
+
     def post(self, request):
+        is_graphql = request.META.get('CONTENT_TYPE') == 'application/graphql'
+
+        if is_graphql:
+            query = request.body.decode()
+        else:
+            form = GrapQLForm(data=request.POST)
+            form.is_valid()
+            query = form.cleaned_data['query']
+
         root_node = RootNode(self.nodes, ctx=request)
         try:
-            data = root_node.query(request.data)
+            data = root_node.query(parse(query))
         except Carbon14Error as e:
-            return Response(
-                {'details': str(e)},
-                status=400
+            data = {'details': str(e)}
+            status = 400
+        else:
+            status = 200
+
+        if is_graphql:
+            return JsonResponse(
+                data,
+                status=status,
+                encoder=self.encoder_class
             )
         else:
-            return Response(data)
+            data = json.dumps(
+                data,
+                cls=self.encoder_class,
+                indent=2
+            )
+            return HttpResponse(
+                content=self.render(answer=data, form=form),
+                status=status,
+            )
+
+    def render(self, **kwargs):
+        return (
+            self.template.render(RequestContext(self.request, kwargs)).encode()
+        )
